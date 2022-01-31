7a2c7491cc955e3ccb079635b0a83914b459123b
eglereact <eglereactjs@gmail.com>
2022-01-31 19:26:47 +0200

    Post component

--- components/Post.js
+++ components/Post.js
@@ -20,6 +20,28 @@ function Post({ username, id, img, userImg, caption }) {
         <p className="flex-1 font-bold">{username}</p>
         <DotsHorizontalIcon className="h-5" />
       </div>
+      <img src={img} alt="" className="w-full object-cover" />
+      <div className="flex justify-between px-4 pt-4">
+        <div className="flex space-x-4">
+          <HeartIcon className="btn" />
+          <ChatIcon className="btn" />
+          <PaperAirplaneIcon className="btn" />
+        </div>
+        <BookmarkIcon className="btn" />
+      </div>
+      <p className="truncate p-5">
+        <span className="mr-1 font-bold">{username} </span>
+        {caption}
+      </p>
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          type="text"
+          placeholder="Add a comment..."
+          className="flex-1 border-none pl-1 outline-none focus:ring-0"
+        />
+        <button className="font-semibold text-blue-400 ">Post</button>
+      </form>
     </div>
   )
 }
