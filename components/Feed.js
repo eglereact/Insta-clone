6433087b1a545922396ee2f995e6e27a2885e7cc
eglereact <eglereactjs@gmail.com>
2022-02-01 17:06:50 +0200

    mini profile

--- components/Feed.js
+++ components/Feed.js
@@ -1,20 +1,23 @@
 import Stories from './Stories'
 import Posts from './Posts'
+import MiniProfile from './MiniProfile'
+import Suggestions from './Suggestions'
 
 function Feed() {
   return (
     <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
       <section className="col-span-2">
-        {/* Stories */}
         <Stories />
-        {/* Posts */}
+
         <Posts />
       </section>
 
-      {/* Section */}
-      <section></section>
-      {/* Mini Profile */}
-      {/* Suggestions */}
+      <section className="hidden md:col-span-1 xl:inline-grid">
+        <div className="fixed top-20">
+          <MiniProfile />
+          <Suggestions />
+        </div>
+      </section>
     </main>
   )
 }
---
+++ components/MiniProfile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function MiniProfile() {
+  return (
+    <div className="mt-14 ml-10 flex items-center justify-between">
+      <img
+        className="h-16 w-16 rounded-full border p-[2px]"
+        src="/images/prof.png"
+        alt=""
+      />
+      <div className="mx-4 flex-1">
+        <h2 className="font-bold">Joe Soe</h2>
+        <h3 className="text-sm" text-gray-400>
+          Welcome to Margatni
+        </h3>
+      </div>
+      <button className="text-sm font-semibold text-blue-400">Sign Out</button>
+    </div>
+  )
+}
+
+export default MiniProfile
