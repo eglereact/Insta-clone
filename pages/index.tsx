f0303e3aa246460fabe6059fd41c9469229740aa
eglereact <eglereactjs@gmail.com>
2022-02-04 16:16:03 +0200

    firebase setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-import Feed from '../components/Feed'
+import Head from 'next/head';
+import Header from '../components/Header';
+import Feed from '../components/Feed';
 
 export default function Home() {
   return (
-    <div className="scrollbar-hide bg-gray-50npm install tailwind-scrollbar-hide h-screen overflow-y-scroll">
+    <div className="bg-gray-50npm install tailwind-scrollbar-hide h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Margatni</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,5 +12,5 @@ export default function Home() {
       <Header />
       <Feed />
     </div>
-  )
+  );
 }
