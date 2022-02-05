5aed1f31d7106c934b518970ee381b94d8fc8e5d
eglereact <eglereactjs@gmail.com>
2022-02-05 18:45:48 +0200

    add upload image modal

--- components/Header.js
+++ components/Header.js
@@ -10,10 +10,13 @@ import {
 import { HomeIcon } from '@heroicons/react/solid';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState } from './../atoms/modalAtom';
 
 function Header() {
   const { data: session } = useSession();
   const router = useRouter();
+  const [open, setOpen] = useRecoilState(modalState);
 
   return (
     <div className="sticky top-0 z-50 border-b bg-white shadow-sm">
@@ -61,7 +64,10 @@ function Header() {
                   3
                 </div>
               </div>
-              <PlusCircleIcon className="navBtn" />
+              <PlusCircleIcon
+                onClick={() => setOpen(true)}
+                className="navBtn"
+              />
               <UserGroupIcon className="navBtn" />
               <HeartIcon className="navBtn" />
               <img
