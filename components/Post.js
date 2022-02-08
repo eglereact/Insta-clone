07801360a22b559e8d463f7a10665aa31247234a
eglereact <eglereactjs@gmail.com>
2022-02-08 16:24:00 +0200

    add comment functionality

--- components/Post.js
+++ components/Post.js
@@ -5,10 +5,32 @@ import {
   EmojiHappyIcon,
   HeartIcon,
   PaperAirplaneIcon,
-} from '@heroicons/react/outline'
-import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+} from '@heroicons/react/outline';
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { db } from '../firebase';
 
 function Post({ username, id, img, userImg, caption }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+
+    const commentToSend = comment;
+    setComment('');
+
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
   return (
     <div className="my-7 rounded-sm border bg-white">
       <div className="flex items-center p-5">
@@ -21,29 +43,43 @@ function Post({ username, id, img, userImg, caption }) {
         <DotsHorizontalIcon className="h-5" />
       </div>
       <img src={img} alt="" className="w-full object-cover" />
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
+
       <p className="truncate p-5">
         <span className="mr-1 font-bold">{username} </span>
         {caption}
       </p>
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="h-7" />
-        <input
-          type="text"
-          placeholder="Add a comment..."
-          className="flex-1 border-none pl-1 outline-none focus:ring-0"
-        />
-        <button className="font-semibold text-blue-400 ">Post</button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Add a comment..."
+            className="flex-1 border-none pl-1 outline-none focus:ring-0"
+          />
+          <button
+            type="submit"
+            disabled={!comment.trim()}
+            onClick={sendComment}
+            className="font-semibold text-blue-400 "
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
